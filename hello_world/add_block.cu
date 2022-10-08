d59a6a937c9469c021383b3e4301a9a571c3f492
maka <makagucci@gmx.com>
2022-10-08 02:46:35 +0100

    [+] wake up sleeper

--- hello_world/add_block.cu
+++ hello_world/add_block.cu
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <math.h>
 
-// # ex. using a single thread
-
-// ## __global__ dunder is a cuda specifier for a kernal function
-// - adds the elements of two arrays
 __global__ void add(int n, float *x, float *y) {
-  for (int i=0; i<n; i++)
+	// cuda c++ provides keywords to return indices of running threads
+  // threadIdx.x contains index of current thread within its block
+  // blockDim.x contains number of threads in the block
+	int index = threadIdx.x;
+	int stride = blockDim.x;
+  for (int i=index; i<n; i+=stride)
 		y[i] = x[i] + y[i];
 }
 
@@ -14,8 +15,8 @@ int main(void) {
 	int N = 1<<20; //20mill elements
 	float *x, *y;	
 	// allocate unified memory accessible from gpu or cpu
-	cudaMallocManaged(&x, N*sizeof(float)); // new's replaced by cudaMallocManaged
-	cudaMallocManaged(&y, N*sizeof(float)); // returns pointer accessible by host, or device code
+	cudaMallocManaged(&x, N*sizeof(float));
+	cudaMallocManaged(&y, N*sizeof(float));
 
 	// initialize x and y arrays on the host (cpu)
 	for (int i=0; i<N; i++) {
@@ -23,11 +24,10 @@ int main(void) {
 		y[i] = 2.0f;
 	}
 
-	// run kernel on 1mill elements of the gpu
-	add<<<1,1>>>(N,x,y); // launch kernel on single thread
-
-	// launching kernel does not block calling cpu thread..
-	// so wait for gpu to finish before accessing results
+	// run kernel on 1mill elements, 256 threads of the gpu
+	add<<<1,256>>>(N,x,y);
+  
+	// wait for gpu to finish before accessing results
 	cudaDeviceSynchronize();
 
 	// check for errors (all values should be 3.0f)
