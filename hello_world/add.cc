05f5a399d9ada6fc8c8f5825351d7237bc7f5123
Maka <makagucci@gmx.com>
2022-10-08 10:35:29 +0000

    [+] fix indentation

--- hello_world/add.cc
+++ hello_world/add.cc
@@ -6,30 +6,30 @@
 // function to add the elements of two arrays
 void add(int n, float *x, float *y) {
   for (int i=0; i<n; i++)
-		y[i] = x[i] + y[i];
+    y[i] = x[i] + y[i];
 }
 int main(void) {
-	int N = 1<<20; //20mill elements
+  int N = 1<<20; //20mill elements
 	
-	float *x = new float[N];
-	float *y = new float[N];
+  float *x = new float[N];
+  float *y = new float[N];
   
-	// initialize x and y arrays on the host (cpu)
-	for (int i=0; i<N; i++) {
+  // initialize x and y arrays on the host (cpu)
+  for (int i=0; i<N; i++) {
     x[i] = 1.0f;
-		y[i] = 2.0f;
-	}
+    y[i] = 2.0f;
+  }
 
-	// run kernel on 1mill elements of the cpu
-	add(N, x, y);
+  // run kernel on 1mill elements of the cpu
+  add(N, x, y);
 	
-	// check for errors (all values should be 3.0f)
+  // check for errors (all values should be 3.0f)
   float maxError = 0.0f;
-	for (int i; i<N; i++) 
-		maxError = fmax(maxError, fabs(y[i]-3.0f));
+  for (int i; i<N; i++) 
+    maxError = fmax(maxError, fabs(y[i]-3.0f));
   std::cout << "Max error: " << maxError << std::endl;
 
-	// free memory
-	delete [] x;
-	delete [] y;
+  // free memory
+  delete [] x;
+  delete [] y;
 }
