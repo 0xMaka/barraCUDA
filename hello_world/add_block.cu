b2de8b53995d5587af6f6f562e0951d95a315221
Maka <makagucci@gmx.com>
2022-10-08 10:30:16 +0000

    [+] fix indentation

--- hello_world/add_block.cu
+++ hello_world/add_block.cu
@@ -2,42 +2,42 @@
 #include <math.h>
 
 __global__ void add(int n, float *x, float *y) {
-	// cuda c++ provides keywords to return indices of running threads
+  // cuda c++ provides keywords to return indices of running threads
   // threadIdx.x contains index of current thread within its block
   // blockDim.x contains number of threads in the block
-	int index = threadIdx.x;
-	int stride = blockDim.x;
+  int index = threadIdx.x;
+  int stride = blockDim.x;
   for (int i=index; i<n; i+=stride)
-		y[i] = x[i] + y[i];
+	  y[i] = x[i] + y[i];
 }
 
 int main(void) {
-	int N = 1<<20; //20mill elements
-	float *x, *y;	
-	// allocate unified memory accessible from gpu or cpu
-	cudaMallocManaged(&x, N*sizeof(float));
-	cudaMallocManaged(&y, N*sizeof(float));
+  int N = 1<<20; //20mill elements
+  float *x, *y;	
+  // allocate unified memory accessible from gpu or cpu
+  cudaMallocManaged(&x, N*sizeof(float));
+  cudaMallocManaged(&y, N*sizeof(float));
 
-	// initialize x and y arrays on the host (cpu)
-	for (int i=0; i<N; i++) {
+  // initialize x and y arrays on the host (cpu)
+  for (int i=0; i<N; i++) {
     x[i] = 1.0f;
-		y[i] = 2.0f;
-	}
+	y[i] = 2.0f;
+  }
 
-	// run kernel on 1mill elements, 256 threads of the gpu
-	add<<<1,256>>>(N,x,y);
+  // run kernel on 1mill elements, 256 threads of the gpu
+  add<<<1,256>>>(N,x,y);
   
-	// wait for gpu to finish before accessing results
-	cudaDeviceSynchronize();
+  // wait for gpu to finish before accessing results
+  cudaDeviceSynchronize();
 
-	// check for errors (all values should be 3.0f)
+  // check for errors (all values should be 3.0f)
   float maxError = 0.0f;
-	for (int i=0; i<N; i++) 
-		maxError = fmax(maxError, fabs(y[i]-3.0f));
+    for (int i=0; i<N; i++) 
+      maxError = fmax(maxError, fabs(y[i]-3.0f));
   std::cout << "Max error: " << maxError << std::endl;
 
-	// free memory
-	cudaFree(x);
-	cudaFree(y);
-	return 0;
+  // free memory
+  cudaFree(x);
+  cudaFree(y);
+  return 0;
 }
