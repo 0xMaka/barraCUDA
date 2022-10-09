751725eed52c3dd422e5f4551e48626c27c46ed0
Maka <makagucci@gmx.com>
2022-10-09 14:38:21 +0000

    [=} fix tabbed indents

--- profiling/utils/getDeviceProperties.cu
+++ profiling/utils/getDeviceProperties.cu
@@ -4,7 +4,7 @@ int main() {
   int nDevices;
   // returns number of cuda capable devices
   cudaError_t err = cudaGetDeviceCount(&nDevices);
-	if (err != cudaSuccess)
+  if (err != cudaSuccess)
     printf("%s\n", cudaGetErrorString(err));
   for (int i=0; i<nDevices; i++) {
     cudaDeviceProp prop;
@@ -14,7 +14,7 @@ int main() {
     printf("Memory Clock Rate (KHz); %d\n", prop.memoryClockRate);
     printf("Memory Bus Width (bits): %d\n", prop.memoryBusWidth);
     printf("Peak Memory Bandwidth (GB/s): %f\n\n", 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
-	}
+  }
 }
 /* ex. Output
 Device Number: 0
