449a5e323763078023d5c4d68384db3673dd5468
Maka <makagucci@gmx.com>
2022-10-10 15:56:08 +0100

    [+] housekeeping

--- hello_world/04-sumArray/sumArray.cu
+++ hello_world/04-sumArray/sumArray.cu
@@ -8,7 +8,7 @@ __global__
 void d_sum_array(float *A, float *B, float *C, const int N) {
   // grid-stride-loop 
   int i = (blockIdx.x * blockDim.x) + threadIdx.x;
-	if (i < N)
+  if (i < N)
     C[i] = A[i] + B[i];
 }
 
@@ -17,11 +17,11 @@ void h_sum_array(float *A, float *B, float *C, const int N) {
   for(int idx=0; idx<N; idx++)
     C[idx] = A[idx] + B[idx];
 }
-
+// random data generator
 void initial_data(float *ip, int size) {
-	time_t t;
-	srand((unsigned int) time(&t));
-	for (int i=0; i<size; i++) {
+  time_t t;
+  srand((unsigned int) time(&t));
+  for (int i=0; i<size; i++) {
     ip[i] = (float) ( rand() & 0xFF )/10.0f;
   }
 }
@@ -33,77 +33,75 @@ void verify(float *href, float *dref, const int N) {
   for (int i=0; i<N; i++) {
     if (abs(href[i] - dref[i]) > threshold) {
       result = 0;
-			printf("[!!] No Match\n");
-			printf("[!!] href: %5.2f dref: %5.2f @ current: %d\n", href[i], dref[i], i);
-			break;
+      printf("[!!] No Match\n");
+      printf("[!!] href: %5.2f dref: %5.2f @ current: %d\n", href[i], dref[i], i);
+      break;
     }
-		if (result)
-			printf("[+] Matching\n");
-		return;
+	if (result)
+	  printf("[+] Matching\n");
+	return;
   }
 }
-  
   
 int main(int argc, char **argv) {
   // set vector size
   int elements = 1024;
   printf("[+] Vector size: %d\n", elements);
 
-	// allocate mem on the cpu
-	size_t bytes = elements * sizeof(float);
-  
-	// host pointers a and b
-	// host reference pointer, gpu reference pointer
-	float *ha, *hb, *href, *dref;
-	ha = (float *)malloc(bytes);
-	hb = (float *)malloc(bytes);
-	href = (float *)malloc(bytes);
+  // allocate mem on the cpu
+  size_t bytes = elements * sizeof(float);
+  // host pointers a and b
+  // host reference pointer, gpu reference pointer
+  float *ha, *hb, *href, *dref;
+  ha = (float *)malloc(bytes);
+  hb = (float *)malloc(bytes);
+  href = (float *)malloc(bytes);
   dref = (float *)malloc(bytes);
 
-	// generate data
+  // generate data
   initial_data(ha, elements);
   initial_data(hb, elements);
   
-	// pad blocks
+  // pad blocks
   memset(dref, 0, bytes);
   memset(href, 0, bytes);
 
-	// allocate global mem on the gpu
-	float *da, *db, *dc;
-	cudaMalloc((float **)&da, bytes);
-	cudaMalloc((float **)&db, bytes);
-	cudaMalloc((float **)&dc, bytes);
+  // allocate global mem on the gpu
+  float *da, *db, *dc;
+  cudaMalloc((float **)&da, bytes);
+  cudaMalloc((float **)&db, bytes);
+  cudaMalloc((float **)&dc, bytes);
 
-	// transfer data to device
+  // transfer data to device
   cudaMemcpy(da, ha, bytes, cudaMemcpyHostToDevice);
   cudaMemcpy(db, hb, bytes, cudaMemcpyHostToDevice);
   cudaMemcpy(dc, dref, bytes, cudaMemcpyHostToDevice);
 
-	// kernel prep
+  // kernel prep
   int len = 512;
-	dim3 block (len);
-	dim3 grid ((elements + block.x -1) / block.x);
-	// run kernel
+  dim3 block (len);
+  dim3 grid ((elements + block.x -1) / block.x);
+  // run kernel
   d_sum_array<<<grid,block>>>(da, db, dc, elements);
   
-	// sync up
-	cudaDeviceSynchronize();
-	// transfer data back from device
+  // sync up
+  cudaDeviceSynchronize();
+  // transfer data back from device
   cudaMemcpy(dref, dc, bytes, cudaMemcpyDeviceToHost);
 
   // redo host side to check against
-	h_sum_array(ha, hb, href, elements);
-	// display result
+  h_sum_array(ha, hb, href, elements);
+  // display result
   verify(href, dref, elements);
 
   // free memory
-	cudaFree(da);
-	cudaFree(db);
-	cudaFree(dc);
-	free(ha);
-	free(hb);
-	free(href);
-	free(dref);
+  cudaFree(da);
+  cudaFree(db);
+  cudaFree(dc);
+  free(ha);
+  free(hb);
+  free(href);
+  free(dref);
 
-	return 0;
+  return 0;
 }
