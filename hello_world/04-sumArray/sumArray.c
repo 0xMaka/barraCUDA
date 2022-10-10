acc994a0a96ae8a2d04a3f7f6c82877f2af48456
Maka <makagucci@gmx.com>
2022-10-10 15:54:52 +0100

    [+] housekeeping

--- hello_world/04-sumArray/sumArray.c
+++ hello_world/04-sumArray/sumArray.c
@@ -8,28 +8,28 @@ void h_sum_array(float *A, float *B, float *C, const int N) {
 }
 
 void initial_data(float *ip, int size) {
-	time_t t;
-	srand((unsigned int) time(&t));
-	for (int i=0; i<size; i++) {
+  time_t t;
+  srand((unsigned int) time(&t));
+  for (int i=0; i<size; i++) {
     ip[i] = (float) ( rand() &0xFF )/10.0f;
   }
 }
 
 int main(int argc, char **argv) {
   int elements = 1024;
-	size_t bytes = elements * sizeof(float);
-	float *ha, *hb, *hc;
-	ha = (float *)malloc(bytes);
-	hb = (float *)malloc(bytes);
-	hc = (float *)malloc(bytes);
+  size_t bytes = elements * sizeof(float);
+  float *ha, *hb, *hc;
+  ha = (float *)malloc(bytes);
+  hb = (float *)malloc(bytes);
+  hc = (float *)malloc(bytes);
 
   initial_data(ha, elements);
   initial_data(hb, elements);
   h_sum_array(ha, hb, hc, elements);
 
-	free(ha);
-	free(hb);
-	free(hc);
+  free(ha);
+  free(hb);
+  free(hc);
 
-	return 0;
+  return 0;
 }
